f2ebe267631446efcfdfba41178489ba3a401f67
azizbekxoshimjonov <azizbekxoshimjonov.veb@gmail.com>
2024-01-22 19:21:57 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ var h1 = document.querySelector('h1'); 
+var age = prompt("Son kiriting")
+if(age ==  18){ 
+    h1. textContent ="Dost"
+}else if(age < 18){ 
+    h1. textContent = "Ukka"
+}else if(age > 18){ 
+    h1. textContent = "Akka"
+}else{
+    h1. textContent = "Togri yozing !!!"
+}
+   
